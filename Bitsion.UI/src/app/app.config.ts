cffc0b52603acda906e9cddbf7d80dea9aafaab4
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-02 15:57:55 -0300

    Add interceptor registration to app.config

--- Bitsion.UI/src/app/app.config.ts
+++ Bitsion.UI/src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: AuthInterceptor, useClass: AuthInterceptor }, // registramos el interceptor en el contenedor DI
+    
   ],
 };
