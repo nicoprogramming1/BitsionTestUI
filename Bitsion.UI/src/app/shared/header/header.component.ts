f058b45898957591b3bbb743151ece47a2acbb13
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-02 20:49:58 -0300

    Create shared header comp

---
+++ Bitsion.UI/src/app/shared/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoutComponent } from '../logout/logout.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [LogoutComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+}
