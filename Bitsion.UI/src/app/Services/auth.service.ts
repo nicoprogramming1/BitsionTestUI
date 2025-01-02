053e69175e5a822aa098f12e663fb89f8f035947
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-02 12:34:01 -0300

    add userRegister to service

--- Bitsion.UI/src/app/Services/auth.service.ts
+++ Bitsion.UI/src/app/Services/auth.service.ts
@@ -3,7 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
 import { AuthStateService } from './auth-state.service';
 import { environment } from './../../environments/environment';
-import { User, UserLoginRequest, UserRegisterRequest, UserResponse } from '../Interfaces/user.interface';
+import { User, UserRegisterRequest, UserResponse } from '../Interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,12 +13,17 @@ export class AuthService {
   private http = inject(HttpClient);
   private authStateService = inject(AuthStateService);
 
-  /** Registro de usuario */
+  /** 
+   * Todos los métodos implementan el state management de auth-state.service.ts
+   * El handleError se encarga de gestionar los errores y sus mensajes
+   */
+
+  
   userRegister(request: UserRegisterRequest): Observable<User | null> {
     this.authStateService.setLoadingState(); // establecemos el estado de loading
     return this.http.post<UserResponse>(`${this.apiUrl}/auth/register`, request).pipe(
       map((res) => {
-        this.authStateService.saveUserState(res); // guardamos el usuario en state y loading: false
+        this.authStateService.setSaveUserState(res); // guardamos el usuario en state y loading: false
         return res;
       }),
       catchError((err) => {
@@ -29,79 +34,10 @@ export class AuthService {
     );
   }
 
-  /** Login de usuario */
-  login(request: UserLoginRequest): Observable<UserResponse | null> {
-    this.authStateService.setLoadingState(); // Establecer el estado de carga
-    return this.http.post<UserResponse>(`${this.apiUrl}/auth/login`, request).pipe(
-      map((res) => {
-        this.storeTokens(res); // Almacenar tokens, si son obtenidos
-        this.authStateService.setLoadingState(); // Terminar estado de carga
-        return res;
-      }),
-      catchError((err) => {
-        this.handleError(err.message); // Manejar error
-        return of(null);
-      })
-    );
-  }
-
-  /** Logout de usuario */
-  logout(): void {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    // Aquí no necesitamos estado de error ni de carga, por lo que no afecta
-  }
-
-  /** Refresh Token */
-  refreshToken(): Observable<UserResponse | null> {
-    const refreshToken = localStorage.getItem('refreshToken');
-    if (!refreshToken) {
-      this.authStateService.setErrorState('No hay token de refresco disponible.'); // Establecer mensaje de error
-      return of(null);
-    }
-    
-    this.authStateService.setLoadingState(); // Establecer estado de carga
-
-    return this.http.post<UserResponse>(`${this.apiUrl}/auth/refresh-token`, { refreshToken }).pipe(
-      map((res) => {
-        this.storeTokens(res); // Almacenar tokens
-        this.authStateService.setLoadingState(); // Terminar estado de carga
-        return res;
-      }),
-      catchError((err) => {
-        this.handleError(err.message); // Manejar error
-        return of(null);
-      })
-    );
-  }
-
-  /** Obtener usuario actual */
-  getCurrentUser(): Observable<UserResponse | null> {
-    this.authStateService.setLoadingState(); // Establecer estado de carga
-    return this.http.get<UserResponse>(`${this.apiUrl}/auth/current-user`).pipe(
-      map((res) => {
-        this.authStateService.setLoadingState(); // Terminar estado de carga
-        return res;
-      }),
-      catchError((err) => {
-        this.handleError(err.message); // Manejar error
-        return of(null);
-      })
-    );
-  }
-
   // este es el manejador de errores del service, le enviamos el mensaje de error
   private handleError(errorMessage: string): void {
     this.authStateService.setErrorState(errorMessage); // establecer el error
   }
 
-  /** Almacenar tokens en localStorage */
-  private storeTokens(res: UserResponse): void {
-    if (res.accessToken) {
-      localStorage.setItem('accessToken', res.accessToken);
-    }
-    if (res.refreshToken) {
-      localStorage.setItem('refreshToken', res.refreshToken);
-    }
-  }
+  
 }
