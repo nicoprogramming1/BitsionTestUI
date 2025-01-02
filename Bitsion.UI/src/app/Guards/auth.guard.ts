cc2f00ec3d1c9bc89c4060517cf36344f8d80560
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-02 16:10:57 -0300

    Add auth guard to protect routes

---
+++ Bitsion.UI/src/app/Guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const isAuthenticated = authService.isAuthenticated();
+  if (!isAuthenticated) {
+    router.navigate(['/login']);
+    return false;
+  }
+  return true;
+};
