fcd830e37df5550ca1fda0798d4ab6b0616c3b7b
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-03 01:06:37 -0300

    Actualize routes and add admin security to register user

--- Bitsion.UI/src/app/app.routes.ts
+++ Bitsion.UI/src/app/app.routes.ts
@@ -1,11 +1,51 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './views/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  
   {
+    title: 'Inicio de sesión',
     path: 'login',
-    component: LoginComponent,
+    loadComponent: () =>
+      import('./views/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
   },
-  //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    title: 'Registro de usuarios',
+    path: 'register',
+    loadComponent: () =>
+      import('./views/auth/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+    canActivate: [authGuard],
+    data: { menu: {  role: 'admin' } }, // sólo es visible para el admin
+  },
+  {
+    title: 'Registro de clientes',
+    path: 'register',
+    loadComponent: () =>
+      import('./views/clients/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    title: 'Clientes',
+    path: 'list',
+    loadComponent: () =>
+      import('./views/clients/list/list.component').then(
+        (m) => m.ListComponent
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    title: 'Consulta de cliente',
+    path: 'client',
+    loadComponent: () =>
+      import('./views/clients/client/client.component').then(
+        (m) => m.ClientComponent
+      ),
+    canActivate: [authGuard],
+  },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
 ];
