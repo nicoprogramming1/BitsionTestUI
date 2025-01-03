e1e0253ab1bbe26c98e9d299b2d3fdda4538b101
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-03 14:02:41 -0300

    Add isLoginRoute validator to hide sidemenu & header in login page

--- Bitsion.UI/src/app/app.component.ts
+++ Bitsion.UI/src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidemenuComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Bitsion.UI';
+  isLoginRoute = false;
+
+  constructor(private router: Router) {
+    // verificar la ruta al navegar
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        // verifica si la ruta actual es login
+        this.isLoginRoute = event.urlAfterRedirects.includes('login');
+      });
+  }
 }
